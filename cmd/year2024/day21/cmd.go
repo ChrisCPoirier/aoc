2e2791898acfc3c40846fa4027a12a3369d56571
Chris Poirier <xxxchrispoirierxxx@gmail.com>
2024-12-30 14:34:43 -0500

    day 21 complete

--- cmd/year2024/day21/cmd.go
+++ cmd/year2024/day21/cmd.go
@@ -3,7 +3,10 @@ package day21
 import (
 	"aoc/cmd/common"
 	"aoc/cmd/grid"
+	"fmt"
 	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -19,8 +22,9 @@ var Cmd = &cobra.Command{
 }
 
 func execute(parent, command string) {
-	common.Run(parent, command, 1, part1, "part 1")
-	common.Run(parent, command, 1, part2, "part 2")
+	// common.Run(parent, command, 1, part1, "part 1 not so optimal")
+	common.Run(parent, command, 1, func(s []byte) int { return part2(s, 2) }, "part 1 using part 2 code")
+	common.Run(parent, command, 1, func(s []byte) int { return part2(s, 25) }, "part 2")
 }
 
 var numPad = grid.Strings{
@@ -36,57 +40,59 @@ var arrowPad = grid.Strings{
 }
 
 func part1(s []byte) int {
-	num := slices.Clone(numPad)
-	sr := 3
-	sc := 2
+	score := 0
+	for _, code := range strings.Split(string(s), "\n") {
+		input := strings.Split(code, ``)
 
-	// input := []string{`0`, `2`, `9`, `A`}
-	input := []string{`0`, `2`, `9`, `A`}
+		num := slices.Clone(numPad)
+		out := sequence(num, input)
+		// cmp := complexity(code, len(out[0]))
+		// logrus.Infof("code %s depth %d optimal is %s len: %d complexity: %d %b", code, 0, strings.Join(out[0], ``), len(out[0]), cmp, cmp)
 
-	a1sr := 0
-	a1sc := 2
-	a2sr := 0
-	a2sc := 2
-	a1string := []string{}
-	final := []string{}
-	for _, in := range input {
-		des := num.FindCell(in)
-		p := num.BFS(sr, sc, des[0], des[1], 0)
-		sr = p.Path[len(p.Path)-1][0]
-		sc = p.Path[len(p.Path)-1][1]
+		for range 2 {
+			new := [][]string{}
+			for _, item := range out {
+				ap := slices.Clone(arrowPad)
+				new = append(new, sequence(ap, item)...)
+			}
 
-		logrus.Infof("num pad %#v", p.Path)
+			min := 0
+			optimal := [][]string{}
+			for _, item := range new {
+				if min == 0 || len(item) < min {
+					min = len(item)
+				}
+			}
 
-		a1 := slices.Clone(arrowPad)
+			for _, item := range new {
+				if len(item) == min {
+					optimal = append(optimal, item)
+				}
+			}
 
-		a1Path := [][][]int{}
-		a1sr, a1sc, a1Path = sequence(a1, a1sr, a1sc, [][][]int{p.Path})
-		logrus.Infof("a1path %#v", a1Path)
-
-		// println(arrowPad.Pretty())
-		// newA1 := [][][]int{}
-		for _, group := range a1Path {
-			p := group[len(group)-1]
-			// newA1 = append(newA1, p)
-			a1string = append(a1string, arrowPad[p[0]][p[1]])
+			out = optimal
+			// cmp := complexity(code, len(out[0]))
+			// logrus.Infof("code %s depth %d optimal is %s len: %d complexity: %d %b", code, i+1, strings.Join(out[0], ``), len(out[0]), cmp, cmp)
 		}
 
-		a2 := slices.Clone(arrowPad)
-
-		a2Path := [][][]int{}
-		logrus.Infof("%#v", a1Path)
-		a2sr, a2sc, a2Path = sequence(a2, a2sr, a2sc, a1Path)
-
-		logrus.Infof("a2path %#v", a2Path)
-
-		for _, group := range a2Path {
-			p := group[len(group)-1]
-			logrus.Infof("group: %#v", group)
-			final = append(final, arrowPad[p[0]][p[1]])
-		}
+		comp := complexity(code, len(out[0]))
+		score += comp
 	}
-	logrus.Infof("%#v", final)
-	return len(`a1string`)
+	return score
+}
+
+func complexity(code string, size int) int {
+	if len(code) < 4 {
+		v, _ := strconv.Atoi(code)
+		return v * size
+	}
+
+	v, err := strconv.Atoi(code[0:3])
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	return v * size
 }
 
 var dm = map[string]string{
@@ -96,30 +102,200 @@ var dm = map[string]string{
 	grid.Key(grid.DIR_DOWN[0], grid.DIR_DOWN[1]):   `v`,
 }
 
-func sequence(g grid.Strings, sr, sc int, groups [][][]int) (int, int, [][][]int) {
-	n := [][][]int{}
-	logrus.Infof("sequence %#v", groups)
-	for _, in := range groups {
-		for i := 0; i < len(in)-1; i++ {
-			dr := in[i+1][0] - in[i][0]
-			dc := in[i+1][1] - in[i][1]
-			k := dm[grid.Key(dr, dc)]
-
-			d := g.FindCell(k)
-
-			logrus.Infof("sr: %d sc: %d d0: %d d1: %d dr: %d dc: %d %s %#v", sr, sc, d[0], d[1], dr, dc, k, d)
-
-			p := g.BFS(sr, sc, d[0], d[1], 0)
-			sr = p.Path[len(p.Path)-1][0]
-			sc = p.Path[len(p.Path)-1][1]
-			n = append(n, p.Path)
-		}
-		n = append(n, [][]int{{0, 2}})
-	}
-	return sr, sc, n
+var dmp = map[string][]int{
+	`<`: {1, 0},
+	`>`: {1, 2},
+	`^`: {0, 1},
+	`v`: {1, 1},
+	`A`: {0, 2},
 }
 
-func part2(s []byte) int {
+func sequence(g grid.Strings, in []string) [][]string {
+	outs := [][]string{}
 
-	return part1(s)
+	cell := g.FindCell(`A`)
+	sr, sc := cell[0], cell[1]
+
+	for _, r := range in {
+		d := g.FindCell(r)
+
+		//if we are already on our destination just hit the activate button again
+		if sr == d[0] && sc == d[1] {
+			outs = cartesian(outs, [][]string{{`A`}})
+			continue
+		}
+
+		paths := g.BFSAll(sr, sc, d[0], d[1], 0, false)
+		sr, sc = d[0], d[1]
+
+		nouts := [][]string{}
+		for _, p := range paths {
+			out := []string{}
+
+			for i := 0; i < len(p.Path)-1; i++ {
+				dr := p.Path[i+1][0] - p.Path[i][0]
+				dc := p.Path[i+1][1] - p.Path[i][1]
+				k := dm[grid.Key(dr, dc)]
+				out = append(out, k)
+			}
+			out = append(out, `A`)
+			nouts = append(nouts, out)
+		}
+		// logrus.Infof("outs: %#v", outs)
+		// logrus.Infof("bfs result: %#v", nouts)
+		outs = cartesian(outs, nouts)
+		// logrus.Infof("catesian: %#v", outs)
+	}
+
+	return outs
+}
+
+func sequence2(g grid.Strings, start, end string) [][]string {
+	outs := [][]string{}
+
+	cellS := g.FindCell(start)
+	sr, sc := cellS[0], cellS[1]
+
+	cellE := g.FindCell(end)
+	er, ec := cellE[0], cellE[1]
+
+	//if we are already on our destination just hit the activate button again
+	if sr == er && sc == ec {
+		outs = cartesian(outs, [][]string{{`A`}})
+		return outs
+	}
+
+	paths := g.BFSAll(sr, sc, er, ec, 0, false)
+
+	nouts := [][]string{}
+	for _, p := range paths {
+		out := []string{}
+
+		for i := 0; i < len(p.Path)-1; i++ {
+			dr := p.Path[i+1][0] - p.Path[i][0]
+			dc := p.Path[i+1][1] - p.Path[i][1]
+			k := dm[grid.Key(dr, dc)]
+			out = append(out, k)
+		}
+		out = append(out, `A`)
+		nouts = append(nouts, out)
+	}
+
+	outs = cartesian(outs, nouts)
+	return outs
+}
+
+func cartesian[T any](sets ...[][]T) [][]T {
+	if len(sets) == 0 {
+		return [][]T{}
+	}
+
+	if len(sets) == 1 {
+		return sets[0]
+	}
+
+	result := sets[0]
+
+	for _, set := range sets[1:] {
+		temp := [][]T{}
+		if len(result) == 0 {
+			result = set
+			continue
+		}
+		for _, element := range set {
+			for _, combinations := range result {
+				temp = append(temp, append(slices.Clone(combinations), slices.Clone(element)...))
+			}
+		}
+
+		result = temp
+	}
+	return result
+}
+
+func part2(s []byte, depth int) int {
+	score := 0
+	for _, code := range strings.Split(string(s), "\n") {
+		input := strings.Split(code, ``)
+
+		num := slices.Clone(numPad)
+		seqs := sequence(num, input)
+
+		optimal := 0
+		for _, seq := range seqs {
+			v := computeKeyPresses(seq, depth)
+
+			if optimal == 0 || v < optimal {
+				optimal = v
+			}
+		}
+
+		comp := complexity(code, optimal)
+		logrus.Infof("code %s len: %d complexity: %d", code, optimal, comp)
+		score += comp
+	}
+	// logrus.Fatal(`test`)
+	return score
+}
+
+func computeKeyPress(s, e []int, depth int, cache map[string]int) int {
+	optimal := 0
+	// optimalSeq := []string{}
+	sr, sc := s[0], s[1]
+	er, ec := e[0], e[1]
+
+	if v, ok := cache[cacheKey(arrowPad[sr][sc], arrowPad[er][ec], depth)]; ok {
+		return v
+	}
+
+	if depth == 0 {
+		return 2
+	}
+
+	if depth == 1 {
+		for _, seq := range sequence2(arrowPad, arrowPad[sr][sc], arrowPad[er][ec]) {
+			possible := len(seq)
+			if optimal == 0 || possible < optimal {
+				// optimalSeq = slices.Clone(seq)
+				optimal = possible
+			}
+		}
+		// logrus.Infof("optimal seq: depth %d, button %s, %#v", depth, arrowPad[sr][sc], optimalSeq)
+		cache[cacheKey(arrowPad[sr][sc], arrowPad[er][ec], depth)] = optimal
+		return optimal
+	}
+
+	for _, seq := range sequence2(arrowPad, arrowPad[sr][sc], arrowPad[er][ec]) {
+		possible := 0
+		seq = append([]string{`A`}, seq...)
+
+		for i := 0; i < len(seq)-1; i++ {
+			possible += computeKeyPress(dmp[seq[i]], dmp[seq[i+1]], depth-1, cache)
+		}
+
+		if optimal == 0 || possible < optimal {
+			// optimalSeq = slices.Clone(seq)
+			optimal = possible
+		}
+	}
+
+	cache[cacheKey(arrowPad[sr][sc], arrowPad[er][ec], depth)] = optimal
+	return optimal
+}
+
+func computeKeyPresses(in []string, depth int) int {
+	comp := 0
+	in = append([]string{`A`}, in...)
+	//start,end,depth
+	cache := map[string]int{}
+
+	for i := 0; i < len(in)-1; i++ {
+		comp += computeKeyPress(dmp[in[i]], dmp[in[i+1]], depth, cache)
+	}
+
+	return comp
+}
+
+func cacheKey(start, end string, depth int) string {
+	return fmt.Sprintf("%s:%s:%d", start, end, depth)
 }
--- cmd/year2024/day21/cmd_test.go
+++ cmd/year2024/day21/cmd_test.go
@@ -11,25 +11,195 @@ func TestParts(t *testing.T) {
 
 	b, err := os.ReadFile(`test.txt`)
 	assert.NoError(t, err)
+	b2, err := os.ReadFile(`test2.txt`)
+	assert.NoError(t, err)
 
 	tests := []struct {
+		name     string
 		expected int
 		input    []byte
 		fn       func([]byte) int
 	}{
 		{
-			expected: 6,
+			name:     `part1`,
+			expected: 126384,
 			input:    b,
 			fn:       part1,
 		},
-		// {
-		// 	expected: 16,
-		// 	input:    b,
-		// 	fn:       part2,
-		// },
+		{
+			name:     `part2`,
+			expected: 126384,
+			input:    b,
+			fn:       func(s []byte) int { return part2(s, 2) },
+		},
+		{
+			name:     `part1 only 0`,
+			expected: 0,
+			input:    []byte(`0`),
+			fn:       part1,
+		},
+		{
+			name:     `part1 only 3`,
+			expected: 36,
+			input:    b2,
+			fn:       part1,
+		},
+		{
+			name:     `part1 only 9`,
+			expected: 126,
+			input:    []byte(`9`),
+			fn:       part1,
+		},
 	}
 
 	for _, test := range tests {
-		assert.Equal(t, test.expected, test.fn(test.input))
+		assert.Equal(t, test.expected, test.fn(test.input), test.name)
+	}
+}
+
+func Test_cartesian(t *testing.T) {
+	tests := []struct {
+		name string
+		sets [][][]any
+		want [][]any
+	}{
+		{
+			name: `2 sets of 2`,
+			sets: [][][]any{
+				{{`1`, `2`}, {`7`, `8`, `9`}},
+				{{`4`, `5`, `6`}, {`11`, `12`, `13`}},
+			},
+			want: [][]any{
+				{`1`, `2`, `4`, `5`, `6`},
+				{`7`, `8`, `9`, `4`, `5`, `6`},
+				{`1`, `2`, `11`, `12`, `13`},
+				{`7`, `8`, `9`, `11`, `12`, `13`},
+			},
+		},
+		{
+			name: `2 set of 1 and 1 set of 2`,
+			sets: [][][]any{
+				{{"^", "^", "^", "A", "v", "A", "<", "<", "A"}},
+				{{"v", ">", "v", ">", "A"}, {"v", ">", ">", "v", "A"}, {">", "v", "v", ">", "A"}, {">", "v", ">", "v", "A"}, {">", ">", "v", "v", "A"}},
+			},
+			want: [][]any{
+				{`^`, `^`, `^`, `A`, `v`, `A`, `<`, `<`, `A`, `v`, `>`, `v`, `>`, `A`},
+				{`^`, `^`, `^`, `A`, `v`, `A`, `<`, `<`, `A`, `v`, `>`, `>`, `v`, `A`},
+				{`^`, `^`, `^`, `A`, `v`, `A`, `<`, `<`, `A`, `>`, `v`, `v`, `>`, `A`},
+				{`^`, `^`, `^`, `A`, `v`, `A`, `<`, `<`, `A`, `>`, `v`, `>`, `v`, `A`},
+				{`^`, `^`, `^`, `A`, `v`, `A`, `<`, `<`, `A`, `>`, `>`, `v`, `v`, `A`},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := cartesian(tt.sets[0], tt.sets[1])
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+// func Test_computeKeyPress(t *testing.T) {
+// 	tests := []struct {
+// 		name  string
+// 		start []int
+// 		want  int
+// 		depth int
+// 	}{
+// 		{
+// 			name:  `press 0 depth 0`,
+// 			start: dmp[`<`],
+// 			depth: 0,
+// 			want:  1,
+// 		},
+// 		{
+// 			name:  `pres 0 depth 1`,
+// 			start: dmp[`<`],
+// 			depth: 1,
+// 			want:  2,
+// 		},
+// 		{
+// 			name:  `pres 0 depth 2`,
+// 			start: dmp[`<`],
+// 			depth: 2,
+// 			want:  8,
+// 		},
+// 		{
+// 			name:  `press 3 depth 0`,
+// 			start: dmp[`^`],
+// 			depth: 0,
+// 			want:  2,
+// 		},
+// 		{
+// 			name:  `press 3 depth 1`,
+// 			start: dmp[`^`],
+// 			depth: 1,
+// 			want:  4,
+// 		},
+// 		{
+// 			name:  `press 3 depth 2`,
+// 			start: dmp[`^`],
+// 			depth: 2,
+// 			want:  12,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got := computeKeyPress(tt.start, tt.depth)
+// 			assert.Equal(t, tt.want, got)
+// 		})
+// 	}
+// }
+
+func Test_computeKeyPresses(t *testing.T) {
+	tests := []struct {
+		name  string
+		in    []string
+		want  int
+		depth int
+	}{
+		// {
+		// 	name:  `0 depth 0`,
+		// 	in:    []string{`<`, `A`},
+		// 	depth: 0,
+		// 	want:  2,
+		// },
+		{
+			name:  `0 depth 1`,
+			in:    []string{`<`, `A`},
+			depth: 0,
+			want:  4,
+		},
+		{
+			name:  `0 depth 2`,
+			in:    []string{`<`, `A`},
+			depth: 2,
+			want:  18,
+		},
+		{
+			name:  `depth 1 `,
+			in:    []string{`<`, `A`, `^`, `A`, `^`, `^`, `>`, `A`, `v`, `v`, `v`, `A`},
+			depth: 1,
+			want:  28,
+		},
+
+		// {
+		// 	name:  `9 depth 0`,
+		// 	in:    []string{`^`, `^`, `^`, `A`},
+		// 	depth: 0,
+		// 	want:  4,
+		// },
+		// {
+		// 	name:  `9 depth 1`,
+		// 	in:    []string{`^`, `^`, `^`, `A`},
+		// 	depth: 1,
+		// 	want:  6,
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := computeKeyPresses(tt.in, tt.depth)
+			assert.Equal(t, tt.want, got)
+		})
 	}
 }
